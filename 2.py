6368e97cea28d551e9677642736f7bb52104e07c
baaalist7 <hgteynasoayn@gmail.com>
2026-05-22 10:34:31 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,15 +1,15 @@
-class userData:
+class UserData:
     def __init__(self, name, age):
         self.user_name = name
         self.age = age
 
-    def printInfo(self):
+    def print_info(self):
         print(f"user:{self.user_name}, age: {self.age}")
 
 
-def process_data(dataList):
+def process_data(data_list):
     result = []
-    for d in dataList:
+    for d in data_list:
         if d.age > 18:
             result.append(d.user_Name.upper())
     return result
